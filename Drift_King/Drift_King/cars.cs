dec80a0832e7535564c6d9f2fa55cd85da75daaa
Luke Arnold <lukearnold@mmc.school.nz>
2019-08-05 15:08:24 +1200

    day1

---
+++ Drift_King/Drift_King/cars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Drift_King
+{
+    class cars
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image carsImage;//variable for the cars image
+
+        public Rectangle carsRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public cars()
+        {
+            x = 10;
+            y = 10;
+            width = 20;
+            height = 20;
+            carsImage = Image.FromFile("car4.png");
+            carsRec = new Rectangle(x, y, width, height);
+        }
+
+    }
+}
---
+++ Drift_King/Drift_King/frmdriftking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drift_King
+{
+    public partial class Driftking : Form
+    
+    {
+
+        public Driftking()
+        {
+            InitializeComponent();
+        }
+
+        private void Driftking_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
